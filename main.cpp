3cd7db7d6628b94f6cddfa22f52d9ed822e545ed
Lipatkin <lipatkin@scaegroup.com>
2023-10-11 18:01:43 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "lib.h"
+
+#include <iostream>
+
+// using namespace std;
+
+int main()
+{
+    std::cout << "Version: " << version() << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    char a;
+    std::cin >> a;
+    
+    return 0;
+}
